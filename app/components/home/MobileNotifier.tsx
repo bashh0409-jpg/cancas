b469018461428f720fa979a56eafe77cda60cc7c
Yanda <bashh0409@gmail.com>
2026-06-29 16:38:30 +0200

    updated the mobile notifier

--- app/components/home/MobileNotifier.tsx
+++ app/components/home/MobileNotifier.tsx
@@ -105,9 +105,8 @@ const MobileNotifier: React.FC<Props> = ({
         <p className="text-xs tracking-tight font-normal mono uppercase max-w-90 mt-1 text-white/60">
           Please use a desktop or tablet to access all features.
         </p>
-      </div>
-
-      <div className="w-full flex flex-col mb-8 justify-center items-center gap-2">
+             <div className="w-full mt-40 flex flex-col mb-8 justify-center items-center gap-2">
+        <span className="text-white mono uppercase  tracking-tight text-[10px] text-center">For now...</span>
         <a
           href="https://www.instagram.com/reflowfyi?igsh=MXRlamY1MHE1ZmxmNA%3D%3D&utm_source=qr"
           target="_blank"
@@ -162,6 +161,8 @@ const MobileNotifier: React.FC<Props> = ({
           </a>{" "}
 
       </div>
+      </div>
+
 
       {/* Bottom social links */}
       <div className="flex hidden items-center justify-center gap-6 px-4 py-6">
